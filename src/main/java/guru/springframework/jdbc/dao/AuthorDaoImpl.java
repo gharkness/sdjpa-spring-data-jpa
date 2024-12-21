535bb16bb4a9443254fbce4ed94dbf07810bdb23
Garrett Harkness <gharkness@sourceallies.com>
2024-12-21 11:51:19 -0600

    add lombok and implement Author Dao

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -1,35 +1,47 @@
 package guru.springframework.jdbc.dao;
 
 import guru.springframework.jdbc.domain.Author;
+import guru.springframework.jdbc.repositories.AuthorRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by jt on 8/28/21.
  */
 @Component
+@AllArgsConstructor
 public class AuthorDaoImpl implements AuthorDao {
+
+    private final AuthorRepository authorRepository;
+
     @Override
     public Author getById(Long id) {
-        return null;
+        return authorRepository.getById(id);
     }
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-        return null;
+        return authorRepository.findAuthorByFirstNameAndLastName(firstName, lastName);
     }
 
     @Override
     public Author saveNewAuthor(Author author) {
-        return null;
+        return authorRepository.save(author);
     }
 
     @Override
+    @Transactional
     public Author updateAuthor(Author author) {
-        return null;
+        Author found = authorRepository.getById(author.getId());
+        found.setFirstName(author.getFirstName());
+        found.setLastName(author.getLastName());
+
+        return authorRepository.save(found);
     }
 
     @Override
     public void deleteAuthorById(Long id) {
-
+        authorRepository.deleteById(id);
     }
 }
